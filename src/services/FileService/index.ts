87b2700b6a553e0e14c3d6c483474b3f74ac0fb9
Codie <ngnhathuy1224@gmail.com>
2024-06-25 07:47:33 +0700

    [TEACHMATE-66] remove unused services

--- src/services/FileService/index.ts
+++
@@ -1,8 +0,0 @@
-export const FileService = {
-  fetchImage: async (url: string, fileName: string) => {
-    const response = await fetch(url);
-    const data = await response.blob();
-
-    return new File([data], fileName, { type: "image/jpeg" });
-  },
-};
